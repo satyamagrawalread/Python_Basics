ee823ae63ca2a1ace1e79d4c29b7b8dacb13b73a
satyamagrawalread <53185604+satyamagrawalread@users.noreply.github.com>
2020-10-01 17:58:14 +0530

    Update q2b_str_comparison.py

--- exercise_files/q2b_str_comparison.py
+++ exercise_files/q2b_str_comparison.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def str_cmp(s1, s2):
+    # pass is used as no code is written inside function
     pass
 
+# string comparison function calling
 assert str_cmp('abc', 'Abc')
 assert str_cmp('Hi there', 'hi there')
 assert not str_cmp('foo', 'food')
